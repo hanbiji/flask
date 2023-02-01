4720ed15362e18161ec179b68b998252cf989c6c
Joe <hanbiji@163.com>
2023-02-01 17:28:29 +0800

    update

--- hello.py
+++ hello.py
@@ -2,12 +2,30 @@
 nginx + flash 部署
 https://www.oschina.net/translate/serving-flask-with-nginx-on-ubuntu
 '''
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = TextAreaField('广告内容?', validators=[DataRequired()])
+    submit = SubmitField('开始生成')
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'DsdflIsdf89'
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return '<h1>Hello World!</h1>'
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
