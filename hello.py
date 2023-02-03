4435d5e7c86e48eb0f0b625c6420122daf4499cd
Joe <hanbiji@163.com>
2023-02-03 17:37:30 +0800

    update language

--- hello.py
+++ hello.py
@@ -77,11 +77,12 @@ def correct():
     ai_data = None
     response = None
     if form.validate_on_submit():
-        response = openai.Edit.create(
-            model="text-davinci-edit-001",
-            input=form.name.data,
-            instruction="Correct this to standard English"
-        )
+        # response = openai.Edit.create(
+        #     model="text-davinci-edit-001",
+        #     input=form.name.data,
+        #     instruction="Correct this to standard English"
+        # )
+        
         # response = openai.Completion.create(
         #     model="text-davinci-003",
         #     prompt="I want you to act as an English translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in English. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level English words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations. My first sentence is:\n\n" + form.name.data,
@@ -91,6 +92,16 @@ def correct():
         #     frequency_penalty=0.0,
         #     presence_penalty=0.0
         # )
+
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt="Correct this to standard English:\n\n" + form.name.data,
+            temperature=0,
+            max_tokens=2048,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0
+        )
         ai_data = response
         name = ai_data['choices'][0]['text']
 
